03286027cb6a80db9a332614a110a5a0fd7099f6
hyraja <paikaray.milan@gmail.com>
2020-12-29 11:52:35 +0530

    work from home hack make always active ur computer

---
+++ Workfromhome_hacks.py
@@ -0,0 +1,13 @@
+# create the .py file and open terminal
+# then install pyautogui in this way "pip install pyautogui"
+# write the python code as below
+
+import pyautogui
+import time
+pyautogui.FAILSAFE = False
+while True:
+    time.sleep(300)
+    for i in range(0, 100):
+        pyautogui.moveTo(0, i*5)
+    for i in range(0, 3):
+        pyautogui.press('shift')
