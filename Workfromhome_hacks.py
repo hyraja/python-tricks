72ade628dc4149bbb600219674463eb8759f17f7
hyraja <paikaray.milan@gmail.com>
2020-12-31 12:29:29 +0530

    work from home hack file modified

--- Workfromhome_hacks.py
+++ Workfromhome_hacks.py
@@ -6,7 +6,7 @@ import pyautogui
 import time
 pyautogui.FAILSAFE = False
 while True:
-    time.sleep(300)
+    time.sleep(200)
     for i in range(0, 100):
         pyautogui.moveTo(0, i*5)
     for i in range(0, 3):
